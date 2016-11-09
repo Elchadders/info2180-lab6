d971ff2fa42b3fe3bab1dfad1e936775908f0ff2
Elchadders <davychd@yahoo.com>
2016-11-09 07:02:46 -0800

    Exercise 1 and 2

---
+++ index.js
@@ -0,0 +1,15 @@
+<!DOCTYPE javascript>
+ 
+$(document).ready(function(){
+  var query = $("#query").val();
+  var link = "https://info2180-lab6-gdavy.c9users.io/request.php?q="+query;
+ 
+  $.ajax(link, {
+    method: "GET",
+  }).done(function(res){
+    alert(res);
+  }).fail(function(err){
+    alert(err);
+  });
+ 
+});
